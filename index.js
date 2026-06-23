4c94f3773f88c3296ee86905b799631d0dcb6cdf
kobietdatten100-sketch <kobietdatten100@gmail.com>
2026-06-23 19:24:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+process.on("unhandledRejection", console.error);
+process.on("uncaughtException", console.error);
+
+client.once("ready", () => {
+  console.log(`✅ ${client.user.tag} đã online`);
+});
+
+client.on("interactionCreate", async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("🏓 Pong!");
+  }
+});
+
+client.login(process.env.TOKEN);
