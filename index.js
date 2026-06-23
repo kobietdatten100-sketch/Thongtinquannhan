c2fb661163cbe4fe7cc56c8c759a613a350a9735
kobietdatten100-sketch <kobietdatten100@gmail.com>
2026-06-23 19:34:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
-const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
+const {
+    Client,
+    GatewayIntentBits,
+    EmbedBuilder
+} = require("discord.js");
+
+const { randomUUID } = require("crypto");
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+    intents: [GatewayIntentBits.Guilds]
 });
 
-process.on("unhandledRejection", console.error);
-process.on("uncaughtException", console.error);
-
 client.once("ready", () => {
-  console.log(`✅ ${client.user.tag} đã online`);
+    console.log(`✅ ${client.user.tag} đã online`);
 });
 
 client.on("interactionCreate", async interaction => {
-  if (!interaction.isChatInputCommand()) return;
+    if (!interaction.isChatInputCommand()) return;
 
-  if (interaction.commandName === "ping") {
-    await interaction.reply("🏓 Pong!");
-  }
+    if (interaction.commandName === "blacklist") {
+
+        const staff = interaction.options.getString("staff");
+        const robloxname = interaction.options.getString("robloxname");
+        const violation = interaction.options.getString("violation");
+
+        const embed = new EmbedBuilder()
+            .setColor("#57F287")
+            .setTitle("User Blacklisted")
+            .setDescription(
+`**Staff:** ${staff}
+
+**UUID:**
+${randomUUID()}
+
+**Target:**
+• Roblox Name: ${robloxname}
+• Violation: ${violation}`
+            )
+            .setTimestamp();
+
+        await interaction.reply({
+            embeds: [embed]
+        });
+    }
 });
 
 client.login(process.env.TOKEN);
